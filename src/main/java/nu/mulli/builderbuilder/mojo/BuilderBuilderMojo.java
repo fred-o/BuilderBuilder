26ca41bbc8fb0e3c79437bce09ab104425a5f40d
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2010-04-07 18:11:24 +0200

    Initial checkin

---
+++ src/main/java/nu/mulli/builderbuilder/mojo/BuilderBuilderMojo.java
@@ -0,0 +1,17 @@
+package nu.mulli.builderbuilder.mojo;
+
+
+import org.apache.maven.plugin.AbstractMojo;
+
+/**
+ * @goal generate-sources
+ * @phase generate-sources
+ */
+public class BuilderBuilderMojo extends AbstractMojo {
+    
+	@Override
+	public void execute() {
+
+	}
+
+}
